060938e9281ef174923ea34dd16e02de8b11094b
Ed Ven <edven7777777@gmail.com>
2024-06-13 11:15:09 +0300

    Error 500 Login

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -1,12 +1,15 @@
 import { Schema, model } from 'mongoose';
 
-const sessionSchema = new Schema({
-  userId: { type: String, required: true },
-  accessToken: { type: String, required: true },
-  refreshToken: { type: String, required: true },
-  accessTokenValidUntil: { type: Date, required: true },
-  refreshTokenValidUntil: { type: Date, required: true },
-});
+const sessionSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
 
 // Створення та експорт моделі на основі визначеної схеми
-export const Session = model('session', sessionSchema);
+export const Session = model('sessions', sessionSchema);
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,15 +1,15 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  // name: Joi.string().min(3).max(20).required().messages({}),
-  // email: Joi.string().min(3).max(20).email().required('bcrypt'),
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().min(3).max(20).email().required(),
+  name: Joi.string().min(3).max(20).required().messages({}),
+  email: Joi.string().min(3).max(20).email().required('bcrypt'),
+  // name: Joi.string().min(3).max(20).required(),
+  // email: Joi.string().min(3).max(20).email().required(),
   password: Joi.string().min(8).max(20).required(),
 });
 
 export const loginUserSchema = Joi.object({
-  // email: Joi.string().min(3).max(20).email().required('bcrypt'),
-  email: Joi.string().min(3).max(20).email().required(),
+  email: Joi.string().min(3).max(20).email().required('bcrypt'),
+  // email: Joi.string().min(3).max(20).email().required(),
   password: Joi.string().min(8).max(20).required(),
 });
